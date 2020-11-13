f8b6166c91355e6e79f04e4ae2f93c03c6dcd3a2
maksktl <matantsev0@gmail.com>
2020-11-13 16:02:54 +0300

    handlers to async

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,3 +1,4 @@
 from .start import dp
+from .steps import dp
 
 __all__ = ["dp"]
--- handlers/users/steps.py
+++ handlers/users/steps.py
@@ -5,20 +5,20 @@ from loader import dp
 
 
 @dp.callback_query_handler(text="bulldozers")
-def bulldozers(call: types.CallbackQuery):
+async def bulldozers(call: types.CallbackQuery):
     await call.message.answer("Выберите номер техники <i>Бульдозер</i>:", reply_markup=bulldozer_num)
 
 
 @dp.callback_query_handler(text="excavator")
-def excavator(call: types.CallbackQuery):
+async def excavator(call: types.CallbackQuery):
     await call.message.answer("Веберите номер техники <i>Эксковаторы</i>:", reply_markup=excavator_num)
 
 
 @dp.callback_query_handler(text_contains="bull:")
-def bull_type(call: types.CallbackQuery):
+async def bull_type(call: types.CallbackQuery):
     await call.message.answer("Выберите ремонт для Бульдозера:", reply_markup=bull_remont_list)
 
 
 @dp.callback_query_handler(text_contains="exc:")
-def exc_type(call: types.CallbackQuery):
+async def exc_type(call: types.CallbackQuery):
     await call.message.answer("Выберите ремонт для Эксковатора:", reply_markup=exc_remont_list)
