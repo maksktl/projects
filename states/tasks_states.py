0e50130939362ed1014b28c1bb05a54d699e0665
maksktl <matansev0@gmail.com>
2021-01-20 22:18:52 +0300

    Change states

--- states/tasks_states.py
+++ states/tasks_states.py
@@ -3,8 +3,8 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 
 class TaskState(StatesGroup):
     name = State()
+    description = State()
+    people = State()
     place = State()
-    date_from = State()
-    date_to = State()
-    time_from = State()
-    time_to = State()
+    date = State()
+    time = State()
