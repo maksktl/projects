b60ddd7e39ba00f12547dc3ba6f0ecf2a626b317
maksktl <matansev0@gmail.com>
2021-01-20 22:14:31 +0300

    Add button for description

--- keyboards/inline/task_manage_keyboard.py
+++ keyboards/inline/task_manage_keyboard.py
@@ -1,11 +1,12 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-async def main(task_name="📃 Название задачи", people="👥 Люди", location="📍 Местоположение", date="📆 День",
+async def main(task_name="📃 Название задачи", task_description="📖 Описание", people="👥 Люди", location="📍 Местоположение", date="📆 День",
                time="⌚️ Время"):
     return InlineKeyboardMarkup(
         inline_keyboard=[
             [InlineKeyboardButton(text=task_name, callback_data="create:task_name")],
+            [InlineKeyboardButton(text=task_description, callback_data="create:task_description")],
             [InlineKeyboardButton(text=people, callback_data="create:people")],
             [InlineKeyboardButton(text=location, callback_data="create:location")],
             [
