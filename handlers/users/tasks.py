2132748ae87821a19118ef85fc7d54fb19041560
maksktl <matantsev0@gmail.com>
2021-01-26 20:13:06 +0300

    Create description

--- handlers/users/tasks.py
+++ handlers/users/tasks.py
@@ -28,6 +28,20 @@ async def ask_name(call: types.CallbackQuery, state: FSMContext):
     await TaskState.name.set()
 
 
+@dp.callback_query_handler(text_contains="create:task_description")
+async def ask_task_description(call: types.CallbackQuery, state: FSMContext):
+    await call.message.answer("📖 Введите описание задачи", reply_markup=back_to_main)
+    await TaskState.description.set()
+
+
+@dp.message_handler(state=TaskState.description)
+async def set_task_description(message: types.Message, state: FSMContext):
+    task_description = message.text
+    await state.update_data(task_description=task_description)
+    await state.reset_state(with_data=False)
+    await add_task(message, state)
+
+
 @dp.message_handler(state=TaskState.name)
 async def set_name(message: types.Message, state: FSMContext):
     task_name = message.text
