ff7c19995953929d5620b3af0b7f032fdea83f28
maksktl <matantsev0@gmail.com>
2021-01-26 19:54:24 +0300

    Remove message within inline keyboard after answer

--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -1,7 +1,9 @@
 from aiogram import Dispatcher
 
+from .middlewares import CallAnswerMiddleware
 from .throttling import ThrottlingMiddleware
 
 
 def setup(dp: Dispatcher):
     dp.middleware.setup(ThrottlingMiddleware())
+    dp.middleware.setup(CallAnswerMiddleware())
